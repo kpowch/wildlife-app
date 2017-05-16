007e92fe5fba869a4624f2c647c400a1d0d2dcb8
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-05-15 20:00:16 -0700

    Initializes react component

---
+++ web/src/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Reactdom from 'react-dom'
+import routes from 'app-root/router'
+
+// this mounts the app in the index.html file body
+const app = document.getElementById('app')
+
+Reactdom.render(
+  <BrowserRouter>{routes}</BrowserRouter>, app
+)
