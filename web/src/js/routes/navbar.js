05172f623c4d72e006943bbae9e51a028bdb5d7f
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-06-18 21:25:08 -0700

    Restyles navbar

--- web/src/js/routes/navbar.js
+++ web/src/js/routes/navbar.js
@@ -24,47 +24,64 @@ class Navbar extends React.Component {
 
     return (
       <nav className='navbar'>
-        <div className='navbar-wrapper'>
-          <div className='navbar-content-left'>
-            <div><Link className='navbar-logo' to='/'>WildLife App</Link></div>
-          </div>
+        <div className='navbar-wrapper-wrapper'>
+          <div className='navbar-wrapper'>
+            <div className='navbar-logo-wrapper navbar-logo-wrapper-mobile'>
+              <div><Link className='navbar-logo text-center' to='/'>
+                <img src='http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder-300x300.png' /></Link>
+              </div>
 
-          <div className='navbar-content-right'>
-            <label className='show-menu'>
-              Show Menu
-              <input type='checkbox' checked={this.state.checked} onChange={this.handleInputChange} />
-            </label>
+              <label className='show-menu'>
+                Show Menu
+                <input type='checkbox' checked={this.state.checked} onChange={this.handleInputChange} />
+              </label>
+            </div>
 
             <ul id='navbar-items' className={menuclass}>
-              <li className='navbar-item'>
-                <Link className='navbar-link' to='/sightings'>Sightings Dashboard <i className='fa fa-angle-down' /></Link>
-                <ul className='navbar-dropdown-items'>
-                  <li className='navbar-dropdown-item'>
-                    <Link className='navbar-link' to='/sightings/add'>Add Sightings</Link>
+              <li className='navbar-item text-left'>
+                <Link className='navbar-link' to='/'>Home</Link>
+              </li>
+              <li className='navbar-item text-left'>
+                <Link className='navbar-link' to='/'>About</Link>
+              </li>
+              <li className='navbar-item text-left'>
+                <Link className='navbar-link' to='/'>Stats</Link>
+              </li>
+
+              <div className='navbar-logo-wrapper navbar-logo-wrapper-browser'>
+                <div><Link className='navbar-logo text-center' to='/'>
+                  <img src='http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder-300x300.png' /></Link>
+                </div>
+              </div>
+
+              <li className='navbar-item text-right'>
+                <Link className='navbar-link' to='/sightings'><i className='fa fa-angle-down' /> Profile</Link>
+                <ul className='navbar-items-dropdown'>
+                  <li className='navbar-item-dropdown'>
+                    <Link className='navbar-link navbar-link-dropdown' to='/sightings/add'>Add Sightings</Link>
                   </li>
-                  <li className='navbar-dropdown-item'>
-                    <Link className='navbar-link' to='/sightings/edit'>Edit Sightings</Link>
+                  <li className='navbar-item-dropdown'>
+                    <Link className='navbar-link navbar-link-dropdown' to='/sightings/edit'>Edit Sightings</Link>
                   </li>
                 </ul>
               </li>
 
-              <li className='navbar-item'>
-                <Link className='navbar-link' to='/who-we-are'>Team <i className='fa fa-angle-down' /></Link>
-                <ul className='navbar-dropdown-items'>
-                  <li className='navbar-dropdown-item'>
-                    <Link className='navbar-link' to='/who-we-are'>Who We Are</Link>
+              <li className='navbar-item text-right'>
+                <Link className='navbar-link' to='/who-we-are'><i className='fa fa-angle-down' /> Contact</Link>
+                <ul className='navbar-items-dropdown'>
+                  <li className='navbar-item-dropdown'>
+                    <Link className='navbar-link navbar-link-dropdown' to='/who-we-are'>Who We Are</Link>
                   </li>
-                  <li className='navbar-dropdown-item'>
-                    <Link className='navbar-link' to='/contact'>Contact</Link>
+                  <li className='navbar-item-dropdown'>
+                    <Link className='navbar-link navbar-link-dropdown' to='/contact'>Contact</Link>
                   </li>
                 </ul>
               </li>
 
-              <li className='navbar-item'>
-                <Link className='navbar-link' to='/signout'>Sign Out</Link>
+              <li className='navbar-item text-right'>
+                <Link className='navbar-link' to='/signout'>Logout</Link>
               </li>
             </ul>
-
           </div>
         </div>
       </nav>
